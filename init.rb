a77353a35c99a9727d11cc6cefcad35cddabdb0d
vividtone <none@none>
2013-07-08 06:37:27 +0900

    initial import.

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_notify_all_fields do
+  name 'Redmine Notify All Fields plugin'
+  author 'MAEDA Go'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'https://www.facebook.com/MAEDA.Go'
+end
